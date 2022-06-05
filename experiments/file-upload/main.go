7d6bdc7ffa494110d907c36676e530a580ddf241
Flavio Silva <flavio.silva@leaseplan.com>
2022-06-05 20:43:29 +0200

    Add test index.html and logs

--- experiments/file-upload/main.go
+++ experiments/file-upload/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
@@ -11,20 +12,25 @@ import (
 
 const MAX_SIZE = 1024 * 1024 * 5 // 5MB
 func upload(w http.ResponseWriter, r *http.Request) {
+	log.Default().Println("Incoming request")
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
+
 	// Check the total size of the request
 	// it's not exactly the size of the file, but prevents parsing the
 	// full body to check the size
+	log.Default().Println("Read body")
 	r.Body = http.MaxBytesReader(w, r.Body, MAX_SIZE)
+	log.Default().Println("Read file")
 	file, handler, err := r.FormFile("myFile")
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(fmt.Sprintf("File not accepted: %q", err)))
 		return
 	}
+	log.Default().Println("Read completed")
 	defer file.Close()
 	tmp, err := os.Create(handler.Filename)
 	if err != nil {
@@ -45,12 +51,16 @@ func upload(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/upload", upload)
-	fmt.Println("Listing on port :9090")
-	http.ListenAndServe("127.0.0.1:9090", nil)
+	log.Default().Println("Listing on port :9090")
+	go func() {
+		if err := http.ListenAndServe("127.0.0.1:9090", nil); err != nil {
+			log.Default().Fatal(err)
+		}
+	}()
 
 	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, syscall.SIGTERM, syscall.SIGINT)
-	<-ch
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
 
-	fmt.Println("Serer sttoped")
+	<-ch
+	log.Default().Println("Serer sttoped")
 }
