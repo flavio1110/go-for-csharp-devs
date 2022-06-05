25a738843fd2eee05de08e5eecfcc55aaf74bfe6
Flavio Silva <flavio.silva@leaseplan.com>
2022-06-05 19:00:15 +0200

    Add file upload experiment

---
+++ experiments/file-upload/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+const MAX_SIZE = 1024 * 1024 * 5 // 5MB
+func upload(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	// Check the total size of the request
+	// it's not exactly the size of the file, but prevents parsing the
+	// full body to check the size
+	r.Body = http.MaxBytesReader(w, r.Body, MAX_SIZE)
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(fmt.Sprintf("File not accepted: %q", err)))
+		return
+	}
+	defer file.Close()
+	tmp, err := os.Create(handler.Filename)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("Unable to create file: %s", err)))
+		return
+	}
+	defer tmp.Close()
+
+	if _, err := io.Copy(tmp, file); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("Unable to copy file: %s", err)))
+		return
+	}
+
+	w.Write([]byte(fmt.Sprintf("Uploaded %q - Size: %d KB", handler.Filename, handler.Size/1024)))
+}
+
+func main() {
+	http.HandleFunc("/upload", upload)
+	fmt.Println("Listing on port :9090")
+	http.ListenAndServe("127.0.0.1:9090", nil)
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGTERM, syscall.SIGINT)
+	<-ch
+
+	fmt.Println("Serer sttoped")
+}
